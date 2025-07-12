6c599e8af7e9686a0f7854410963749003ae81b9
Ahamefula Chibundu <ahamefulachibundu@gmail.com>
2025-07-12 11:29:28 +0100

    Modified authentication

--- authentication/authenticated.js
+++ authentication/authenticated.js
@@ -1,24 +1,23 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 dotenv.config();
 
 const authenticated = (req, res, next) => {
-    const session = req.session?.user;
-    if (!session) {
-        return res.status(401).json({message: "Unauthorized access"});
-    }
+  const session = req.headers.authorization;
 
-    const token = session.token;
-    if (!token) {
-        return res.status(401).json({message: "Unauthorized access"});
-    }
+  if (!session || !session.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized access - no token" });
+  }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(403).json({message: "Invalid token"});
-    }
+  const token = session.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid or expired token" });
+  }
 };
+
 export default authenticated;
